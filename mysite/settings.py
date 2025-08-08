a2543f29eea60424ba1187644805a058662dd64b
Glory <geaakoto@st.ug.edu.gh>
2025-08-08 17:58:05 +0000

    Sidebar creation and auth pages mod

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,9 +123,35 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+# STATIC_URL = "/static/"
+# STATICFILES_DIRS = [BASE_DIR / 'polls/static']
+# STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# # settings.py
+
+# # Disable collectstatic during development
+# if DEBUG:
+#     STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+#     STATIC_ROOT = None   
+
+# settings.py
+
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [BASE_DIR / 'polls/static']
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+# Development-only settings
+if DEBUG:
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static',           
+        BASE_DIR / 'polls/static',      
+    ]
+    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+    STATIC_ROOT = None  # Disable collectstatic during development
+else:
+    # Production settings
+    STATIC_ROOT = BASE_DIR / 'staticfiles'  # Where collectstatic will copy files
+    STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
